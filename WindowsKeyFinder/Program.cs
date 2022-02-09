4463dde2b75a0a3b87d6953f15c094e0b3efbde2
kenjitagawa <kenjitagawa.kt@gmail.com>
2022-02-08 22:07:49 -0500

    Add project files.

---
+++ WindowsKeyFinder/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace WindowsKeyFinder
+{
+    class Program
+       {
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+
+            Windows productKey = new Windows();
+
+            productKey.FindProductKeyClick();
+
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+
+
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            Console.WriteLine("|\tWelcome to Windows Product Key Finder!                                      |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|\tHere is your Windows Key:                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine($"|                            {productKey.Key}                          |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                                   |");
+            Console.WriteLine("|                                                                    - Kenji T.     |");
+            Console.WriteLine("-------------------------------------------------------------------------------------");
+            
+
+
+
+            
+            Console.Write("Press any key to quit.");
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ WindowsKeyFinder/Windows.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsKeyFinder
+{
+    class Windows
+    {
+        #region Constants
+
+        // first byte offset
+        private const int START = 52;
+
+        // last byte offset
+        private const int END = START + 15;
+
+        // length of decoded key
+        private const int LENGTH = 29;
+
+        private const int DECODED = 15;
+
+        private string m_productKey;
+
+        // List of characters in bytes
+        private List<byte> keyHex = new List<byte>();
+
+        // List of possible alpha-numeric characters in key
+        List<char> keyChars = new List<char>()
+        {
+            'B', 'C', 'D', 'F', 'G', 'H',
+            'J', 'K', 'M', 'P', 'Q', 'R',
+            'T', 'V', 'W', 'X', 'Y', '2',
+            '3', '4', '6', '7', '8', '9'
+        };
+
+        // Array of characters to hold the product key decoded
+        char[] decodedKey = new char[LENGTH];
+
+        #endregion
+
+        #region Properties
+
+        public string Key 
+        {
+            get 
+            {
+                return m_productKey;
+            }
+        }
+
+
+        #endregion
+
+
+        public string DecodeKey(byte[] id)
+        {
+            //add all bytes to our list
+            for (var i = START; i <= END; i++)
+                keyHex.Add(id[i]);
+
+
+            //now the decoding starts
+            for (var i = LENGTH - 1; i >= 0; i--)
+
+                switch ((i + 1) % 6)
+                {
+                    //if the calculation is 0 (zero) then add the seperator
+                    case 0:
+                        decodedKey[i] = '-';
+                        break;
+                    default:
+                        var idx = 0;
+
+                        for (var j = DECODED - 1; j >= 0; j--)
+                        {
+                            var @value = (idx << 8) | keyHex[j];
+                            keyHex[j] = (byte)(@value / 24);
+                            idx = @value % 24;
+                            decodedKey[i] = keyChars[idx];
+                        }
+                        break;
+                }
+
+            return new string(decodedKey);
+
+        }
+
+        public void FindProductKeyClick()
+        {
+            byte[] id = null;
+            var regKey = Registry.LocalMachine.OpenSubKey("SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion");
+
+            if (regKey != null) id = regKey.GetValue("DigitalProductId") as byte[];
+
+            m_productKey = DecodeKey(id);
+        }
+
+    }
+}
